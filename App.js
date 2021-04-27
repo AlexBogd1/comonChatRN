ed4b887ca7f64c910c6806cb4b8fee38b3711b92
Alex <7894664@mail.ru>
2021-04-27 15:11:19 +0300

    add started libraries and app structure

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const App = () => {
   return (
     <View>
-      <Text>Initialstate</Text>
+      <Text>Initial state</Text>
     </View>
   );
 };
